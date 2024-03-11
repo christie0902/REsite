e584f8933e5a1b162807ac45362ecfff7a0b7b18
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-03-11 19:36:38 +0100

    seed orders table

---
+++ database/seeders/OrdersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class OrdersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        $statuses = ['pending', 'completed', 'cancelled', 'refunded'];
+
+        for ($i = 2; $i <= 11; $i++) {
+            DB::table('orders')->insert([
+                'user_id' => $i,
+                'total_price' => $faker->randomFloat(2, 10, 500), // Generate a float between 10 and 500
+                'status' => $faker->randomElement($statuses),
+                'created_at' => $faker->dateTimeBetween('-1 month', 'now'),
+                'updated_at' => $faker->dateTimeBetween('-1 month', 'now'),
+            ]);
+        }
+    }
+}
