25e6df70642c2e9f37c2752da36ce0d96a8749b6
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-03-11 17:20:53 +0100

    finish products list

--- resources/views/admin/products/list.blade.php
+++ resources/views/admin/products/list.blade.php
@@ -3,59 +3,18 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="{{ asset('css/list.css') }}">
 <title>Product Table</title>
-<style>
-    body {
-      font-family: 'Arial', sans-serif;
-      background-color: #f4f4f4;
-    }
-    table {
-      width: 100%;
-      border-collapse: collapse;
-      background-color: #fff;
-      margin-top: 20px;
-    }
-    th, td {
-      text-align: left;
-      padding: 12px;
-      border-bottom: 1px solid #ddd;
-    }
-    th {
-      background-color: #f8f8f8;
-    }
-    .action-icons {
-      display: flex;
-      justify-content: flex-start;
-    }
-    .action-icons img {
-      margin-right: 5px;
-      cursor: pointer;
-      width: 50px;
-    }
-    .stock {
-      text-align: center;
-    }
-    .price {
-      text-align: right;
-      padding-right: 20px;
-    }
-    .pagination {
-      margin-top: 15px;
-      display: flex;
-      justify-content: center;
-    }
-    svg {
-        width: 30px;
-        margin-bottom: -10px;
-    }
-    div a {
-        margin:10px;
-        padding-left: 10px;
-        text-decoration: none;
-    }
-  </style>
 </head>
 <body>
+  <div class="top-bar">
+    <form action="" method="post">
+    <input type="text" id="search-product" placeholder="Search in products" />
+    <button>Search</button>
+  </form>
+    <button id="add-product">+ Add Product</button>
+    <button id="import-csv">Import CSV</button>
+</div>
 <table>
   <thead>
     <tr>
@@ -72,7 +31,10 @@
     @foreach ($products as $product)
         <tr>
             <td><input type="checkbox"></td>
-            <td>{{htmlspecialchars($product["name"])}}</td>
+            <td>
+              <img src={{$product["image_url"]}} alt="Product Image" class="product-image">
+              {{ htmlspecialchars($product["name"]) }}
+          </td>
             <td>{{htmlspecialchars($product["category"]["name"])}}</td>
             <td class="stock">{{htmlspecialchars($product["stock_quantity"])}}</td>
             <td class="price">${{htmlspecialchars($product["price"])}}</td>
