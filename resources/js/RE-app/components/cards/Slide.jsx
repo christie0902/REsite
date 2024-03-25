7282c36cae8978d7ebc33a1bd284d641838e39eb
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-03-25 17:06:45 +0100

    make changes to the category section

--- resources/js/RE-app/components/cards/Slide.jsx
+++ resources/js/RE-app/components/cards/Slide.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
+import CustomButton from "../buttons/CustomButton";
 
 const Slider = ({
-    categoryName2,
+    header,
     description2,
     description1,
     categoryName1,
@@ -34,8 +35,11 @@ const Slider = ({
             onMouseLeave={handleMouseLeave}
         >
             <div className="slider-2">
-                <h2>{categoryName2}</h2>
-                <p>{description2}</p>
+            <h1 className="mb-4 text-center text-4xl font-bold text-yellow-500 md:mb-6 md:text-4xl mt-5">Check out our Shop now
+                </h1>
+                <p className="mb-6 text-2xl text-gray-300">Explore our amazing models, realistic collectibles,
+                    impressive costume and a lot more in our shop! Explore our shop today and get the best collections of all fans!</p>
+                <CustomButton type="static" title="Shop Now"/>
             </div>
             <div
                 className="slider-image"
@@ -51,8 +55,27 @@ const Slider = ({
                 }}
             ></div>
             <div className="slider-1">
-                <h2>{categoryName1}</h2>
-                <p>{description1}</p>
+                <h1 className="mb-4 text-center text-4xl font-bold text-yellow-500 md:mb-6 md:text-4xl mt-5">
+                    {header}
+                </h1>
+                <p className="mb-6 text-2xl text-gray-300">
+                    Explore our amazing models, realistic collectibles,
+                    impressive costume and a lot more in our shop! Explore our shop today and get the best collections of all fans!
+                </p>
+                <ul className="text-left mt-10">
+                    <li className="mb-3 flex items-center ">
+                        <span className="mr-2">🎮</span> Models
+                    </li>
+                    <li className="mb-3 flex items-center ">
+                        <span className="mr-2">🎮</span> Gifts & Collectibles
+                    </li>
+                    <li className="mb-3 flex items-center ">
+                        <span className="mr-2">🎮</span> Customized T-shirt
+                    </li>
+                    <li className="flex items-center ">
+                        <span className="mr-2">🎮</span> Apparels
+                    </li>
+                </ul>
             </div>
         </section>
     );
