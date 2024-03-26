303fe5119df0583b363bfe0d2f903db077f64f80
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-03-26 10:15:05 +0100

    styling category section on homepage

--- resources/js/RE-app/components/cards/Slide.jsx
+++ resources/js/RE-app/components/cards/Slide.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import CustomButton from "../buttons/CustomButton";
+import { Link } from "react-router-dom";
 
-const Slider = ({
-    header,
-    description2,
-    description1,
-    categoryName1,
-    bgImage1,
-    bgImage2,
-}) => {
+const Slider = () => {
+    const bgImage1="https://res.cloudinary.com/diwszstai/image/upload/v1711380644/site-assets/category_img_nmyn62.png";
+    const bgImage2="https://res.cloudinary.com/diwszstai/image/upload/v1711443635/site-assets/category_img2_osja1d.png"
+
     const [bgStyle, setBgStyle] = useState({
         backgroundImage: `url(${bgImage1})`,
         left: "0%",
     });
+
+    
 
     const handleMouseEnter = () => {
         setBgStyle({
@@ -35,11 +34,8 @@ const Slider = ({
             onMouseLeave={handleMouseLeave}
         >
             <div className="slider-2">
-            <h1 className="mb-4 text-center text-4xl font-bold text-yellow-500 md:mb-6 md:text-4xl mt-5">Check out our Shop now
-                </h1>
-                <p className="mb-6 text-2xl text-gray-300">Explore our amazing models, realistic collectibles,
-                    impressive costume and a lot more in our shop! Explore our shop today and get the best collections of all fans!</p>
-                <CustomButton type="static" title="Shop Now"/>
+            <h1 className="text-6xl font-bold text-yellow-500 mb-4 md:text-7xl font-bebas-neue">LET'S EXPLORE OUR COLLECTION!</h1>
+            <Link to="/shop"><button class="bg-red-800 text-white px-5 py-2 rounded-md shadow-md hover:shadow-lg transition duration-300 glassmorphism-button mt-5">Shop Now</button></Link>
             </div>
             <div
                 className="slider-image"
@@ -55,16 +51,16 @@ const Slider = ({
                 }}
             ></div>
             <div className="slider-1">
-                <h1 className="mb-4 text-center text-4xl font-bold text-yellow-500 md:mb-6 md:text-4xl mt-5">
-                    {header}
+                <h1 className="mb-4 text-center text-4xl font-bold text-yellow-500 md:mb-6 md:text-4xl mt-5 font-bebas-neue">
+                    Categories
                 </h1>
-                <p className="mb-6 text-2xl text-gray-300">
+                <p className="text-md text-gray-300">
                     Explore our amazing models, realistic collectibles,
                     impressive costume and a lot more in our shop! Explore our shop today and get the best collections of all fans!
                 </p>
                 <ul className="text-left mt-10">
                     <li className="mb-3 flex items-center ">
-                        <span className="mr-2">🎮</span> Models
+                        <span className="mr-2 ">🎮</span> Models
                     </li>
                     <li className="mb-3 flex items-center ">
                         <span className="mr-2">🎮</span> Gifts & Collectibles
--- resources/js/RE-app/pages/Home.jsx
+++ resources/js/RE-app/pages/Home.jsx
@@ -2,15 +2,10 @@ import React, { useContext, useEffect } from "react";
 import BlogSection from "../components/BlogSection";
 import Newsletter from "../components/Newsletter";
 import Slide from "../components/cards/Slide";
-import CategorySection from "../components/CategorySection";
-import Hero from "../components/Hero";
-import FeaturedProducts from "../components/FeaturedProducts";
 import Carousel from "../components/carousel/Carousel";
 import CustomizerSection from "../components/CustomizerSection";
 import Context from "../store/Context.js";
 
-import collectibles from "../assets/website-design/product.png";
-import model from "../assets/categories/collectibles4.jpg";
 
 const Home = () => {
     const { state, dispatch } = useContext(Context);
@@ -36,15 +31,7 @@ const Home = () => {
         <div className="homepage">
             <Carousel />
             <CustomizerSection />
-            <Slide
-            header="Categories"
-            description1=" Explore our amazing models, realistic collectibles,
-            impressive costume and a lot more in our shop!"
-            categoryName2="COLLECTIBLES"
-            description2="Collection of props and items with differnt theme"
-            bgImage1="https://res.cloudinary.com/diwszstai/image/upload/v1711380644/site-assets/category_img_nmyn62.png"
-            bgImage2="https://res.cloudinary.com/diwszstai/image/upload/v1711382404/site-assets/category_img1_ctutm3.png"
-            />
+            <Slide/>
             {/* <CategorySection /> */}
             <BlogSection />
             <Newsletter />
